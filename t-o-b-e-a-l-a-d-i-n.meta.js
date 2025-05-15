3c917956fde17d77e80bb67ae6980821be2be006
kawasakisora2345 <kawasakisora2345@github.com>
2025-05-15 18:52:58 +0700

    init

--- t-o-b-e-a-l-a-d-i-n.meta.js
+++ t-o-b-e-a-l-a-d-i-n.meta.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         t-o-b-e-a-l-a-d-i-n
 // @namespace    t-o-b-e-a-l-a-d-i-n.user.js
-// @version      2025.05.15
+// @version      2025.05.15.1
 // @description  down t-o-b-e-a-l-a-d-i-n
 // @author       You
 // @include      /^https:\/\/t[o0]b[e3]\.[a4]l[a4]d[i1]n\.c[o0]\.kr\/n\/\w+/
 // @icon         https://raw.githubusercontent.com/kawaguchitakumi8902/buc_tien_hoa/main/t-o-b-e-a-l-a-d-i-n.png
 // @grant        none
-// @require      https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.7.53/dist/zip.min.js
+// @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
 // @downloadURL  https://github.com/kawaguchitakumi8902/buc_tien_hoa/raw/main/t-o-b-e-a-l-a-d-i-n.user.js
 // @updateURL    https://github.com/kawaguchitakumi8902/buc_tien_hoa/raw/main/t-o-b-e-a-l-a-d-i-n.meta.js
 // ==/UserScript==
